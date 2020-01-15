2aa2c558a4031013bdab37d134a3ae7c7b11b100
Leonardo Ribeiro <leonardo.ribeiro@omnilink.com.br>
2020-01-15 20:33:34 -0300

    Refactor Code

---
+++ src/presentation/routers/login-router.js
@@ -0,0 +1,16 @@
+const httpResponse = require('../helpers/http-response')
+
+module.exports = class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return httpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return httpResponse.badRequest('email')
+    }
+    if (!password) {
+      return httpResponse.badRequest('password')
+    }
+  }
+}
